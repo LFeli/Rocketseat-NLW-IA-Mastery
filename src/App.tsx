f954df2bea4b918b4abb9eabf1d7573f1ce6951c
Lucas Felipe <lucasfelipemk@gmail.com>
2023-09-12 11:35:28 -0300

    feat: content session done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import { Github } from "lucide-react";
+import { FileVideo, Github, Upload, Wand2 } from "lucide-react";
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
+import { Label } from "./components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
+import { Slider } from "./components/ui/slider";
 
 export function App() {
   return (
@@ -21,6 +25,30 @@ export function App() {
         </div>
       </div>
 
+      {/* Main */}
+      <main className="flex flex-1 gap-6 p-6">
+
+        {/* Content */}
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea 
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Inclua o prompt para a IA..."
+            />
+
+            <Textarea 
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Resultado gerado pela IA..." 
+              readOnly
+              />
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: Você pode utilizar a variável <code className="text-violet-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado
+          </p>
+        </div>
+
+      </main>
     </div>
   )
 }
