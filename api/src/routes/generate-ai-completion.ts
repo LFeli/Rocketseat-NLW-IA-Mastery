84e2a21a1f3690ac156258b6e0256e0b1af37317
Lucas Felipe <lucasfelipemk@gmail.com>
2023-09-13 16:00:33 -0300

    feat: 'generate-ai-complete' route done

--- api/src/routes/generate-ai-completion.ts
+++ api/src/routes/generate-ai-completion.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { openai } from "../lib/openai";
 
 export async function generationIACompletionRoute(app: FastifyInstance){
-  app.post('/ai/complete', async (req) => {
+  app.post('/ai/complete', async (req, reply) => {
 
     const bodySchema = z.object({
       videoID: z.string().uuid(),
@@ -12,10 +14,26 @@ export async function generationIACompletionRoute(app: FastifyInstance){
 
     const { videoID, template, temperature } = bodySchema.parse(req.body)
 
-    return {
-      videoID, 
-      template, 
-      temperature,
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoID,
+      }
+    })
+
+    if(!video.transcription){
+      return reply.status(400).send({ error: 'Vídeo transcription was not generated yet.' })
     }
+
+    const promptMessage = template.replace('{transcripition}', video.transcription)
+
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo-16k',
+      temperature,
+      messages: [
+        { role: 'user', content: promptMessage }
+      ],
+    })
+
+    return response
   })
 }
