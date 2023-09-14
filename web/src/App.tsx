b695adcd569913949b4626b0839468b91260986f
Lucas Felipe <lucasfelipemk@gmail.com>
2023-09-14 11:06:20 -0300

    picking values of prompts in DB

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,8 +6,15 @@ import { Label } from "./components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
 import { Slider } from "./components/ui/slider";
 import { VideoInputForm } from "./components/video-input-form";
+import { PromptSelect } from "./components/prompt-select";
 
 export function App() {
+  
+
+  function handlePromptSelected(template: string){
+    console.log(template)
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       {/* Header */}
@@ -58,17 +65,7 @@ export function App() {
           <form className="space-y-6">
             <div className="space-y-2">
               <Label>Prompt</Label>
-
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt.." />
-                </SelectTrigger>
-
-                <SelectContent>
-                  <SelectItem value="title">Título do YouTube</SelectItem>
-                  <SelectItem value="description">Descrição do YouTube</SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect onPromptSelected={handlePromptSelected}/>
             </div>
             
             <div className="space-y-2">
